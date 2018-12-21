322b68e506d9c18f1d8eae3476c34ea6b7dd3793
Jarek <jarekgab1@gmail.com>
2018-12-21 19:42:23 +0100

    Changes in CityWeatherActivity, new SectionsPageAdapter

---
+++ app/src/main/java/pl/nauka/jarek/weather/Tab1Fragment.java
@@ -0,0 +1,21 @@
+package pl.nauka.jarek.weather;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Tab1Fragment extends Fragment {
+    private static String TAG = "Tab1Fragment";
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.tab1_fragment, container, false);
+
+        return view;
+    }
+}
---
+++ app/src/main/java/pl/nauka/jarek/weather/Tab2Fragment.java
@@ -0,0 +1,21 @@
+package pl.nauka.jarek.weather;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Tab2Fragment extends Fragment {
+    private static String TAG = "Tab2Fragment";
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.tab2_fragment, container, false);
+
+        return view;
+    }
+}
